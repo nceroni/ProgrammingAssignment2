9c26a76634061cb2a841bb5fd92728ec8ac81ca5
nceroni <nceroni@gmail.com>
2015-01-23 17:40:22 -0300

    Revert "Finished Assignement"
    This reverts commit 3ecc57b8acdde55c77c66064da640b87393abeab.

--- cachematrix.R
+++ cachematrix.R
@@ -1,58 +1,15 @@
-## These function are a pair of functions that cache the inverse of a matrix. 
+## Put comments here that give an overall description of what your
 ## functions do
 
-## This function creates a special "matrix" object that can cache its inverse.
+## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-  # m is the inverse that is going to be cached
-  m <- NULL
-  # set function is for setting the matrix in cache
-  set <- function(y) {
-    x <<- y
-    m <<- NULL
-  }
-  # get function is for get the cache matrix
-  get <- function() x
-  
-  # this function calculate the inverse of the matrix and store in cache
-  setinverse <- function(inverse) m <<- inverse
-  
-  # this function get the inverse of the matrix setied in the cache
-  getinverse <- function() m
-  
-  # the list of the functions 
-  list(set = set, get = get,
-       setinverse = setinverse,
-       getinverse = getinverse)
-  
+
 }
 
 
-## his function computes the inverse of the special "matrix" returned by makeCacheMatrix above. 
-## If the inverse has already been calculated (and the matrix has not changed), 
-## then the cachesolve should retrieve the inverse from the cache.
+## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
-    
-    # This get the return of the inverse of the matrix
-    m <- x$getinverse()
-    # this if return the inverse, if its exist (distinct of NULL) and then finish the function
-    if(!is.null(m)) {
-      message("getting cached data")
-      return(m)
-    }
-    
-    
-    # If dont exist the inverse function, this get the matrix stored in the cache
-    data <- x$get()
-    
-    # this calculate the inverse of the matrix
-    m <- solve(data)
-    
-    # this store the inverse of the matrix in the cache for next execution
-    x$setinverse(m)
-    
-    #return the inverse of the matrix
-    m
 }
